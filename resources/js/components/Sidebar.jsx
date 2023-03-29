2ecb768d5b317271c885adcba82f850610f9fe67
Rastislav Madera <maderarasto@gmail.com>
2023-03-30 01:55:07 +0200

    Adds a user panel in which are information about signed-in user.

--- resources/js/components/Sidebar.jsx
+++ resources/js/components/Sidebar.jsx
@@ -1,14 +1,15 @@
 import {useState, useEffect} from 'react';
-
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBasketShopping, faHouse, faSoap} from "@fortawesome/free-solid-svg-icons";
+import {faBasketShopping, faHouse, faSoap} from '@fortawesome/free-solid-svg-icons';
 
 import MenuSheetItem from './MenuSheetItem';
 import MenuOverviewItem from './MenuOverviewItem';
-import logo from "../../img/smartbudget_logo.png";
+import UserPanel from './UserPanel';
+
+import logo from '../../img/smartbudget_logo.png';
 
 export default function Sidebar() {
     const [budgetSheets, setBudgetSheets] = useState([]);
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
         setBudgetSheets([
@@ -16,7 +17,12 @@ export default function Sidebar() {
             { name: 'August 2023', active: false },
             { name: 'July 2023', active: false },
             { name: 'June 2023', active: false },
-        ])
+        ]);
+
+        setUser({
+            name: 'Rastislav',
+            email: 'maderarasto@gmail.com'
+        });
     }, []);
 
     function handleSheetItemClick(sheetIndex) {
@@ -38,8 +44,8 @@ export default function Sidebar() {
             <div className="sidebar__logo">
                 <img src={logo} alt="SmartBudget logo" />
             </div>
-            <div className="sidebar__sheets">
-                <small className="fw-bold text-uppercase text-secondary">Your budget sheets</small>
+            <div className="sidebar__section sheets">
+                <small className="title">Your budget sheets</small>
                 <div className="sheets-list">
                     {budgetSheets.map(({name, active}, index) => (
                         <MenuSheetItem
@@ -49,15 +55,17 @@ export default function Sidebar() {
                     ))}
                 </div>
             </div>
-            <div className="sidebar__overview">
-                <small className="fw-bold text-uppercase text-secondary">Monthly overview</small>
+            <div className="sidebar__section overview">
+                <small className="title">Monthly overview</small>
                 <div className="overview-list">
                     <MenuOverviewItem name="Groceries" icon={faBasketShopping} budget={150} sum={123.5} currency="EUR" />
                     <MenuOverviewItem name="Household" icon={faHouse} budget={50} sum={11.2} currency="EUR" />
                     <MenuOverviewItem name="Hygiene" icon={faSoap} budget={50} sum={33.9} currency="EUR" />
                 </div>
             </div>
-            <div className="sidebar__user"></div>
+            <div className="sidebar__user">
+                <UserPanel user={user} />
+            </div>
         </div>
     )
 }
