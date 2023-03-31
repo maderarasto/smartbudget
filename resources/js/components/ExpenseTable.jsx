ef02b3e47fda143ee098371a8270296119d3ec91
Rastislav Madera <maderarasto@gmail.com>
2023-04-01 00:32:56 +0200

    Added expense table component thatbesides table  contains  also action button, search box and paginator.

---
+++ resources/js/components/ExpenseTable.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faAngleLeft, faAngleRight, faMagnifyingGlass, faPlus} from "@fortawesome/free-solid-svg-icons";
+
+export default function ExpenseTable({columns, data, pagination, total}) {
+    function renderTableHeader({key, label, style}) {
+        const element = key !== 'id' ? 'span' : 'input';
+        const child = element === 'span' ? React.createElement(element, {}, label) :
+            React.createElement(element, {
+                type: 'checkbox',
+                className: 'form-check-input'
+            });
+
+        return React.createElement('th', {
+            key: key,
+            style: style
+        }, child);
+    }
+
+    function renderTableCell(expenseItem, {key: columnKey, style: columnStyle}) {
+        const element = columnKey === 'id' ? 'th' : 'td';
+        const child = columnKey !== 'id' ? expenseItem[columnKey] : React.createElement('input', {
+            type: 'checkbox',
+            className: 'form-check-input'
+        });
+
+        return React.createElement(element, {
+            key: columnKey,
+            style: columnStyle
+        }, child);
+    }
+
+    return (
+        <div className="expense-table">
+            <div className="expense-table__toolbar">
+                <button className="btn btn-add-expense">
+                    <FontAwesomeIcon icon={faPlus} />
+                    <span>Add new expense</span>
+                </button>
+
+                <form>
+                    <div className="input-with-icon">
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        <input type="text" className="form-control" placeholder="Search..." />
+                    </div>
+                </form>
+            </div>
+            <table className="table">
+                <thead>
+                    <tr>{columns.map(column => renderTableHeader(column))}</tr>
+                </thead>
+                <tbody>
+                {
+                    data.map(expenseItem => (
+                        <tr key={expenseItem.id}>
+                            {columns.map(column => renderTableCell(expenseItem, column))}
+                        </tr>
+                    ))
+                }
+                </tbody>
+            </table>
+
+            <div className="expense-table__footer">
+                <div className="table-paginator">
+                    <button className="btn btn-sm">
+                        <FontAwesomeIcon icon={faAngleLeft} />
+                    </button>
+                    <div className="table-paginator__page btn btn-sm">1 of 10</div>
+                    <button className="btn btn-sm">
+                        <FontAwesomeIcon icon={faAngleRight} />
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
